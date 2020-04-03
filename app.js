fd43fdebc227069e69b4b19d19e6b70519257125
Max <max@Songs-MacBook-Pro.local>
2020-04-03 23:14:56 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var list = document.getElementById('list');
+var input = document.getElementById('input-field');
+
+let LIST = [];
+let id = 0;
+
+input.addEventListener("keyup", function(event) {
+    if(event.keyCode === 13) {
+        event.preventDefault();
+        addToListArray();
+        input.value = "";
+    }
+})
+
+function addToListArray() {
+    var input = document.getElementById('input-field').value;
+    LIST.push({
+        name: input,
+        id: id    
+    });
+    updateList(input, id, false);
+    id++;
+    this.input.value = "";
+    console.log(LIST);
+}
+
+function updateList(item, id, done) {
+    console.log(item);
+    console.log(id);
+    console.log(done);
+    const text = `<li id="${id}" class="item">
+                    <div>
+                        <p style="display: inline-block" class = "text"> ${item} </p>
+                        <button onclick=removeFromList(${id})> Remove </button>
+                    </div>
+                </li>`;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, text);
+}
+
+function removeFromList(id) {
+    var element = document.getElementById(id);
+    element.parentNode.removeChild(element);
+    LIST.splice(id, 1);
+    console.log(LIST[id]);
+    console.log('remove');
+}
+
