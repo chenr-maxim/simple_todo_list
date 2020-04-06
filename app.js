947a52aaafe5a619c4a1f007d5081a8e70fd0eb8
Maxim Chen <chenr.maxim@gmail.com>
2020-04-07 02:33:21 +0800

    Fixed remove from list; added css

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var list = document.getElementById('list');
 var input = document.getElementById('input-field');
 
 let LIST = [];
+let id = 0;
 
 input.addEventListener("keyup", function(event) {
     if(event.keyCode === 13) {
@@ -12,7 +13,6 @@ input.addEventListener("keyup", function(event) {
 })
 
 function addToListArray() {
-    let id = 0;
     var input = document.getElementById('input-field').value;
     LIST.push({
         name: input,
@@ -23,20 +23,29 @@ function addToListArray() {
     this.input.value = "";
 }
 
-function updateList(item, id, done) {
+function updateList(inputName, id) {
     const text = `<li id="${id}" class="item">
                     <div>
-                        <p style="display: inline-block" class = "text"> ${item} </p>
+                        <p style="display: inline-block" class="text"> ${inputName} </p>
                         <button onclick=removeFromList(${id})> Remove </button>
                     </div>
                 </li>`;
     const position = "beforeend";
     list.insertAdjacentHTML(position, text);
+    console.log(LIST);
+    console.log("finished adding item");
 }
 
 function removeFromList(id) {
+    if( id == LIST.slice(-1)[0].id) {
+        console.log('popped');
+        LIST.pop();
+    } else {
+        LIST.splice(id,1);
+    }
     var element = document.getElementById(id);
     element.parentNode.removeChild(element);
-    LIST.splice(id, 1);
+    console.log('finished removing from list');
+    console.log(LIST);
 }
 
